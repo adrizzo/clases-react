999f3fae9463b890effdeb140812469211718513
Andres <zplaguee@gmail.com>
2021-02-17 20:25:27 -0500

    First component and React Setup

---
+++ src/Components/Titles/Titles.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Titles.css'
+
+const Titles = () => {
+  return (
+    <>
+      <h1>Hola H1 Paola Y Aleja Y Andres que tiene 31 años y esta viejo</h1>
+    </>
+  )
+}
+
+export default Titles
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Titles from '../Components/Titles/Titles'
+
+function Home() {
+  return (
+    <div>
+      <Titles>lajsdhajlhsdajd</Titles>
+    </div>
+  )
+}
+
+export default Home
